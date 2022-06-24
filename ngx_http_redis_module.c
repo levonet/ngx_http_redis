28263d8d23d94c91d23d66699467f91944120c36
Pavlo Bashynskiy <pavlo.bashynskyi@blablacar.com>
2022-06-24 18:09:56 +0300

    bugfix: fixed build error with nginx >= 1.23.0

--- ngx_http_redis_module.c
+++ ngx_http_redis_module.c
@@ -581,7 +581,7 @@ found:
             ngx_str_set(&h->key, "Content-Encoding");
             ngx_str_set(&h->value, "gzip");
             h->lowcase_key = (u_char*) "content-encoding";
-#if (NGX_HTTP_GZIP)
+#if (NGX_HTTP_GZIP) && defined(nginx_version) && nginx_version < 1023000
             u->headers_in.content_encoding = h;
 #endif
         }
